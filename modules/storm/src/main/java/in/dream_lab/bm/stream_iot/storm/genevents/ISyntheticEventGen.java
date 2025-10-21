c6bac1a8e44c3b278e52d3a1d8a838943b5730cd
edmondsylar <edmondmusiitwa@gmail.com>
2025-10-21 06:00:09 +0300

    Clean up TTPyRIoT branch: Remove Java benchmark files (docs, modules, pom.xml)

--- modules/storm/src/main/java/in/dream_lab/bm/stream_iot/storm/genevents/ISyntheticEventGen.java
+++
@@ -1,7 +0,0 @@
-package in.dream_lab.bm.stream_iot.storm.genevents;
-
-import java.util.List;
-
-public interface ISyntheticEventGen {
-	public void receive(List<String> event);  //event
-}
--- modules/tasks/src/main/java/in/dream_lab/bm/stream_iot/tasks/Defaults.java
+++
@@ -1,6 +0,0 @@
-package in.dream_lab.bm.stream_iot.tasks;
-
-public class Defaults {
-
-	public static final String OBS_TYPE = "obsType";
-}
